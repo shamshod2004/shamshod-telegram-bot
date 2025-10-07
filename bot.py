190620457c6a6c29bf68542bbb2f3c1bcf0a4072
shamshod2004 <shamshodhamidov801@gmail.com>
2025-10-07 06:51:16 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -990,6 +990,35 @@ async def send_video(message: types.Message):
         )
         await message.answer("Botdan foydalanish uchun quyidagi kanallarga obuna bo‘ling:", reply_markup=markup)
 
+@dp.message(F.text == "41")
+async def send_video(message: types.Message):
+    user_id = message.from_user.id
+    
+    if await check_subs(user_id):  # Faqat obuna bo‘lganlarga javob qaytaradi
+        file_id = "BAACAgUAAxkBAAIDamjkceOABVTZxS6rQYnvQRnjIZW8AALTGQACuWHwVgWjjQyGOBoBNgQ"
+        await message.answer_video(file_id, caption="""
+
+🎥Nomi: Drakula
+➖➖➖➖➖➖➖➖➖➖
+🌍Tili: Oʻzbek Tilida 
+📀Sifati: 1080P Mobile HD
+🌏Davlat: AQSH
+📆Yili:  2025-yil
+🎞️Janri: #Jangari #urush #dramma  #romantika #melodrama #fantastik 
+        ⌨️ KOD: #41
+       bot; @UrtakKino_bot
+
+        kanal; @Tarjima_kinolar_uzb_tilda_z""")
+                                                        
+    else:
+        await message.answer('telegram kanalga obuna boling')
+        markup = InlineKeyboardMarkup(
+            inline_keyboard=[
+                [InlineKeyboardButton(text=f"🔗 {channel}", url=f"https://t.me/{channel[1:]}")] for channel in CHANNELS
+            ] + [[InlineKeyboardButton(text="✅ Tekshirish", callback_data="check_subs")]]
+        )
+        await message.answer("Botdan foydalanish uchun quyidagi kanallarga obuna bo‘ling:", reply_markup=markup)
+
 
 
 
