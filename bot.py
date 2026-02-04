ece8397df0e74cd547ce02d40d8f02463933a482
shamshod2004 <shamshodhamidov801@gmail.com>
2026-02-04 12:38:24 +0500

    Add video sending functionality for specific commands

--- bot.py
+++ bot.py
@@ -2023,6 +2023,46 @@ async def send_video(message: types.Message):
             ] + [[InlineKeyboardButton(text="✅ Tekshirish", callback_data="check_subs")]]
         )
         await message.answer("Botdan foydalanish uchun quyidagi kanallarga obuna bo‘ling:", reply_markup=markup)
+   @dp.message(F.text == "79")
+async def send_video(message: types.Message):
+    user_id = message.from_user.id
+    
+    if await check_subs(user_id):  # Faqat obuna bo‘lganlarga javob qaytaradi
+        file_id = "BAACAgQAAxkBAAIH0mmC9yg0oEa5B3EHiuVghqJhF8cSAAJLHQAC6GX4U5GsboZnRuEyOAQ"
+        await message.answer_video(file_id, caption="""
+🗂 Yuklash: 6510
+        ⌨️ KOD: #79
+       bot; @UrtakKino_bot
+        kanal; @Tarjima_kinolar_uzb_tilda_z""")
+                                                        
+    else:
+        await message.answer('telegram kanalga obuna boling')
+        markup = InlineKeyboardMarkup(
+            inline_keyboard=[
+                [InlineKeyboardButton(text=f"🔗 {channel}", url=f"https://t.me/{channel[1:]}")] for channel in CHANNELS
+            ] + [[InlineKeyboardButton(text="✅ Tekshirish", callback_data="check_subs")]]
+        )
+        await message.answer("Botdan foydalanish uchun quyidagi kanallarga obuna bo‘ling:", reply_markup=markup)
+     @dp.message(F.text == "80")
+async def send_video(message: types.Message):
+    user_id = message.from_user.id
+    
+    if await check_subs(user_id):  # Faqat obuna bo‘lganlarga javob qaytaradi
+        file_id = "BAACAgQAAxkBAAIH1GmC910tipm8fu59WStnZ1Ok0M2AAAJ1HAACtLtxU4hcKzZqfMGyOAQ"
+        await message.answer_video(file_id, caption="""
+🗂 Yuklash: 6510
+        ⌨️ KOD: #80
+       bot; @UrtakKino_bot
+        kanal; @Tarjima_kinolar_uzb_tilda_z""")
+                                                        
+    else:
+        await message.answer('telegram kanalga obuna boling')
+        markup = InlineKeyboardMarkup(
+            inline_keyboard=[
+                [InlineKeyboardButton(text=f"🔗 {channel}", url=f"https://t.me/{channel[1:]}")] for channel in CHANNELS
+            ] + [[InlineKeyboardButton(text="✅ Tekshirish", callback_data="check_subs")]]
+        )
+        await message.answer("Botdan foydalanish uchun quyidagi kanallarga obuna bo‘ling:", reply_markup=markup)
     
 async def main():
     print('bot ishladi....')
